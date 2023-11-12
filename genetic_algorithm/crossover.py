f8dab8f2069e81714366d2cbacc5989e09c002f8
Gaby-D <gdorsch_24@hotmail.com>
2023-11-12 13:24:39 -0300

    Comentarios añadidos en genetic

--- genetic_algorithm/crossover.py
+++ genetic_algorithm/crossover.py
@@ -2,38 +2,48 @@
 import numpy as np
 from typing import Tuple
 
+# Se definen diferentes metodos para realizar las cruzas de los individuos
 def simulated_binary_crossover(parent1: np.ndarray, parent2: np.ndarray, eta: float) -> Tuple[np.ndarray, np.ndarray]:
     """
-    This crossover is specific to floating-point representation.
-    Simulate behavior of one-point crossover for binary representations.
-    For large values of eta there is a higher probability that offspring will be created near the parents.
-    For small values of eta, offspring will be more distant from parents
-    Equation 9.9, 9.10, 9.11
+    Define un metodo de cruza orientado a representaciones de punto flotante, simulando el metodo de
+    cruza one-point para representaciones binarias
+    Para grandes valores de eta, hay mayor probabilidad de que el hijo sea creado mas parecido a los padres
+    Para pequeños valores de eta, hay mayor probabilidad de que el hijo sea creado menos parecido a los padres
+    Ecuacion 9.9, 9.10, 9.11
     """    
-    # Calculate Gamma (Eq. 9.11)
+    # Calculo de Gamma (Ec. 9.11)
     rand = np.random.random(parent1.shape)
     gamma = np.empty(parent1.shape)
-    gamma[rand <= 0.5] = (2 * rand[rand <= 0.5]) ** (1.0 / (eta + 1))  # First case of equation 9.11
-    gamma[rand > 0.5] = (1.0 / (2.0 * (1.0 - rand[rand > 0.5]))) ** (1.0 / (eta + 1))  # Second case
+    gamma[rand <= 0.5] = (2 * rand[rand <= 0.5]) ** (1.0 / (eta + 1))  # Primer caso de la ecuacion 9.11
+    gamma[rand > 0.5] = (1.0 / (2.0 * (1.0 - rand[rand > 0.5]))) ** (1.0 / (eta + 1))  # Segundo caso de la ec
 
-    # Calculate Child 1 chromosome (Eq. 9.9)
+    # Calculo del cromosoma hijo 1 (Ec. 9.9)
     chromosome1 = 0.5 * ((1 + gamma)*parent1 + (1 - gamma)*parent2)
-    # Calculate Child 2 chromosome (Eq. 9.10)
+    # Calculo del cromosoma hijo 2 (Ec. 9.10)
     chromosome2 = 0.5 * ((1 - gamma)*parent1 + (1 + gamma)*parent2)
 
     return chromosome1, chromosome2
 
 def uniform_binary_crossover(parent1: np.ndarray, parent2: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Define el metodo de cruza donde se crea una mascara con 0 y 1.
+    Dependiendo como se defina la funcion, al recorrer los genes de los hijos y la mascara, se realiza un swap
+    o no entre los genes de los hijos dependiendo el valor de la mascara
+    """
     offspring1 = parent1.copy()
     offspring2 = parent2.copy()
     
-    mask = np.random.uniform(0, 1, size=offspring1.shape)
-    offspring1[mask > 0.5] = parent2[mask > 0.5]
-    offspring2[mask > 0.5] = parent1[mask > 0.5]
+    mask = np.random.uniform(0, 1, size=offspring1.shape) # Mascara con 0 y 1, del tamaño del individuo
+    offspring1[mask > 0.5] = parent2[mask > 0.5] # Swap del gen
+    offspring2[mask > 0.5] = parent1[mask > 0.5] # Swap del gen
 
     return offspring1, offspring2
 
 def single_point_binary_crossover(parent1: np.ndarray, parent2: np.ndarray, major='r') -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Define el metodo de cruza one-point donde se elige un punto del cromosoma de los padres y se hace un swap de
+    los bits para crear a los hijos. Aca hace algo mas complicado y no lo entiendo
+    """
     offspring1 = parent1.copy()
     offspring2 = parent2.copy()
 
--- genetic_algorithm/mutation.py
+++ genetic_algorithm/mutation.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import List, Union, Optional
 from .individual import Individual
 
-# Define diferentes metodos para realizar la mutacion
+# Se definen diferentes metodos para realizar la mutacion de los individuos
 def gaussian_mutation(chromosome: np.ndarray, prob_mutation: float, 
                       mu: List[float] = None, sigma: List[float] = None,
                       scale: Optional[float] = None) -> None:
@@ -35,7 +35,7 @@ def random_uniform_mutation(chromosome: np.ndarray, prob_mutation: float,
     Si un gen es seleccionado, se le asignara un valor con una probabilidad uniforme entre [low,high)
     [low, high) esta definido para cada gen para abarcar todo el rango posible de valores
     """
-    assert type(low) == type(high), 'low and high must have the same type'
+    assert type(low) == type(high), 'low and high deben ser del mismo tipo'
     mutation_array = np.random.random(chromosome.shape) < prob_mutation
     if isinstance(low, list):
         uniform_mutation = np.random.uniform(low, high)
@@ -53,6 +53,9 @@ def uniform_mutation_with_respect_to_best_individual(chromosome: np.ndarray, bes
     chromosome[mutation_array] += uniform_mutation[mutation_array] * (best_chromosome[mutation_array] - chromosome[mutation_array])
 
 def cauchy_mutation(individual: np.ndarray, scale: float) -> np.ndarray:
+    """
+    No implementado
+    """
     pass
 
 def exponential_mutation(chromosome: np.ndarray, xi: Union[float, np.ndarray], prob_mutation: float) -> None:
@@ -61,30 +64,30 @@ def exponential_mutation(chromosome: np.ndarray, xi: Union[float, np.ndarray], p
     Mutacion exponencial basada en funciones que no se de donde saca
     """
     mutation_array = np.random.random(chromosome.shape) < prob_mutation
-    # Fill xi if necessary
+    # Rellena xi si es necesario
     if not isinstance(xi, np.ndarray):
         xi_val = xi
         xi = np.empty(chromosome.shape)
         xi.fill(xi_val)
 
-    # Change xi so we get E(0, 1), instead of E(0, xi)
+    # Cambia xi para obtener E(0, 1), en vez de E(0, xi)
     xi_div = 1.0 / xi
     xi.fill(1.0)
     
-    # Eq 11.17
+    # Ec 11.17
     y = np.random.uniform(size=chromosome.shape)
     x = np.empty(chromosome.shape)
     x[y <= 0.5] = (1.0 / xi[y <= 0.5]) * np.log(2 * y[y <= 0.5])
     x[y > 0.5] = -(1.0 / xi[y > 0.5]) * np.log(2 * (1 - y[y > 0.5]))
 
-    # Eq 11.16
+    # Ec 11.16
     delta = np.empty(chromosome.shape)
     delta[mutation_array] = (xi[mutation_array] / 2.0) * np.exp(-xi[mutation_array] * np.abs(x[mutation_array]))
 
-    # Update delta such that E(0, xi) = (1 / xi) * E(0 , 1)
+    # Actualizacion del delta E(0, xi) = (1 / xi) * E(0 , 1)
     delta[mutation_array] = xi_div[mutation_array] * delta[mutation_array]
 
-    # Update individual
+    # Actualizacion del individuo
     chromosome[mutation_array] += delta[mutation_array]
 
 def mmo_mutation(chromosome: np.ndarray, prob_mutation: float) -> None:
@@ -94,12 +97,12 @@ def mmo_mutation(chromosome: np.ndarray, prob_mutation: float) -> None:
     """
     from scipy import stats
     mutation_array = np.random.random(chromosome.shape) < prob_mutation
-    normal = np.random.normal(size=chromosome.shape)  # Eq 11.21
-    cauchy = stats.cauchy.rvs(size=chromosome.shape)  # Eq 11.22
+    normal = np.random.normal(size=chromosome.shape)  # Ec 11.21
+    cauchy = stats.cauchy.rvs(size=chromosome.shape)  # Ec 11.22
     
-    # Eq 11.20
+    # Ec 11.20
     delta = np.empty(chromosome.shape)
     delta[mutation_array] = normal[mutation_array] + cauchy[mutation_array]
 
-    # Update individual
+    # Actualizacion del individuo
     chromosome[mutation_array] += delta[mutation_array]
